0e23489bb937b0867a0b5901563247c44a83ee7e
werjiaweiwen <651769329@qq.com>
2019-11-14 17:09:52 +0800

    jww

---
+++ 贾伟雯/api-test-2019/src/com/edu/freemarker/FreemarkerTemplateEngine.java
@@ -0,0 +1,47 @@
+package com.edu.freemarker;
+
+import java.io.File;
+import java.io.StringWriter;
+import java.util.Locale;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+
+//FreeMarker模板引擎
+public class FreemarkerTemplateEngine {
+	//模板路径
+	private static final String DEFAULT_TEMPLATE = "test-output/jww/demo.html";   
+    public String getTemplatePath() {   
+        return "";   
+    }   
+ 
+       
+    public String run(Map context) throws Exception{   
+    	return executeFreemarker(context); 
+    }   
+    
+    private String executeFreemarker(Map context)throws Exception{   
+    	String content="";
+        Configuration cfg = new Configuration();   
+        //加载模板文件
+        cfg.setDirectoryForTemplateLoading(new File(getTemplatePath())); 
+        //编码方式
+        cfg.setEncoding(Locale.CHINA, "UTF-8");
+        // 设置模板检索数据模型的方式
+        cfg.setObjectWrapper(new DefaultObjectWrapper());              
+        cfg.setCacheStorage(new freemarker.cache.MruCacheStorage(20, 250));                      
+        Template temp = cfg.getTemplate(getTemplate());    
+        StringWriter out = new StringWriter();   
+        temp.process(context, out);   
+        System.out.print(out.toString());
+        return out.toString();
+    }   
+  
+    public String getTemplate() {   
+        // TODO Auto-generated method stub   
+        return DEFAULT_TEMPLATE;   
+    }
+    
+}
